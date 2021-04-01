aa8765d74c798475c571ae5bc1320d8e7993a116
Berrak Ozer <bo2220@columbia.edu>
2021-04-01 16:49:01 +0300

    update on the code

--- src/citation_vs_reviewscore.py
+++ src/citation_vs_reviewscore.py
@@ -9,7 +9,24 @@ file_path = data_path + input_file
 
 with open(file_path,mode='r',encoding='utf-8') as file:
     data = list(csv.reader(file,delimiter=';'))
-file.close()
 
-print(type(data),len(data))
-print(data[0])
+header = data[0]
+for i in range(0,len(header)):
+    if 'citations' in header[i]:
+        citation_index = i
+
+    elif 'Citations' in header[i]:
+        citation_index = i
+
+    elif 'Review scores' in header[i]:
+        review_scores_index = i
+
+citations,review_scores  = [],[]
+
+for i in range(1,len(data)):
+    if data[i][review_scores_index] != '':
+        citations.append(int(data[i][citation_index]))
+        review_scores.append(data[i][review_scores_index])
+
+print(type(data[1][review_scores_index]))
+print(len(review_scores),len(citations))
