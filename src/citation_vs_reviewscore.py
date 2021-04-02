0776f469525b652ef6c77be51517b4554076fd77
Berrak Ozer <bo2220@columbia.edu>
2021-04-02 18:11:07 +0300

    made the plot and implemented the Path approach

--- src/citation_vs_reviewscore.py
+++ src/citation_vs_reviewscore.py
@@ -1,11 +1,21 @@
+#import section
 import csv
 import os
 import sys
+import matplotlib.pyplot as plt
+from pathlib import Path
+#---------------------------------------------------------
+#input section
+INPUT_FILE_NAME = 'acta_a_citations_review_scores_utf8.csv'
 
-src_path = os.getcwd()
-data_path = src_path.strip('src') + 'data/'
-input_file = 'acta_a_citations_review_scores_utf8.csv'
-file_path = data_path + input_file
+#---------------------------------------------------------
+
+
+src_path = Path.cwd()
+parent_path = src_path.resolve().parent
+data_path = parent_path / 'data'
+
+file_path = data_path / INPUT_FILE_NAME
 
 with open(file_path,mode='r',encoding='utf-8') as file:
     data = list(csv.reader(file,delimiter=';'))
@@ -26,7 +36,16 @@ citations,review_scores  = [],[]
 for i in range(1,len(data)):
     if data[i][review_scores_index] != '':
         citations.append(int(data[i][citation_index]))
-        review_scores.append(data[i][review_scores_index])
+        review_scores.append(float(data[i][review_scores_index].split()[0]))
+        #print(review_scores)
+        #sys.exit()
+#print(type(data[1][review_scores_index]))
+#print(len(review_scores),len(citations))
 
-print(type(data[1][review_scores_index]))
-print(len(review_scores),len(citations))
+plt.xlim(1,5)
+plt.ylim(0,145)
+plt.scatter(review_scores, citations)
+plt.show()
+
+
+
